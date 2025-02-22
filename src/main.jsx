24752f3cde3ac2345213f04a2343ff3c359f6c72
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-02-22 20:06:18 +0200

    js

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store,} from "./redux/store";
 import App from "./components/App/App";
 import "modern-normalize";
 import "./index.css";
@@ -9,7 +9,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <App />
     </Provider>
   </React.StrictMode>
 );
